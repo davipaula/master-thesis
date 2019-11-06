130e855facbd58cf91b2a781ea32e27faf80ee76
Davi Paula <dnascimentodepau@wayfair.com>
2019-11-06 13:30:10 +0100

    Refactoring dataset creation

--- han_wiki_data/src/dataset_creation.py
+++ han_wiki_data/src/dataset_creation.py
@@ -3,10 +3,10 @@ import json
 import pandas as pd
 
 
-def read_click_stream_dump(cs_dump_path):
+def get_click_stream_dump(click_stream_dump_path):
     rows = []
 
-    with open(cs_dump_path, 'r') as f:
+    with open(click_stream_dump_path, 'r') as f:
         for i, line in enumerate(f):
             cols = line.split('\t')
 
@@ -22,11 +22,11 @@ def read_click_stream_dump(cs_dump_path):
     return pd.DataFrame(rows, columns=['prev', 'current', 'n'])
 
 
-def get_available_titles(docs_path):
+def get_available_titles(wiki_documents_path):
     # Load preprocessed
     title2sects = {}
 
-    with open(docs_path, 'r') as f:
+    with open(wiki_documents_path, 'r') as f:
         for i, line in enumerate(f):
             doc = json.loads(line)
             title = doc['title'].replace(' ', '_')
@@ -37,12 +37,7 @@ def get_available_titles(docs_path):
     return set(title2sects.keys())  # save as set (makes it faster)
 
 
-def normalize_dataset(cdf, available_titles):
-    # Clicks for that we have matching articles
-    fdf = cdf[(cdf['prev'].isin(available_titles)) & (cdf['current'].isin(available_titles))].copy()
-
-    print(f'Click pairs with articles: {len(fdf):,}')
-
+def add_metrics(fdf):
     max_n = fdf.groupby(['prev']).agg({'n': 'max'})
 
     # Normalize click count with max value
@@ -54,33 +49,72 @@ def normalize_dataset(cdf, available_titles):
     return fdf
 
 
-def add_articles_data(data_path, fdf):
-    texts, labels, articles = [], [], []
+def filter_available_titles(click_stream, available_titles):
+    filtered_dataset = click_stream[
+        (click_stream['prev'].isin(available_titles)) & (click_stream['current'].isin(available_titles))].copy()
 
-    with open(data_path, 'r') as json_file:
-        json_list = list(json_file)
+    return filtered_dataset
 
-    for json_str in json_list:
-        result = json.loads(json_str)
 
-        texts.append(result['sections'][0]['paragraphs'])
-        articles.append(result['title'])
+def generate_click_stream_dataset(click_stream_dump_path, dataset_path):
+    click_stream = get_click_stream_dump(click_stream_dump_path)
+    available_titles = get_available_titles(dataset_path)
 
-    articles_df = pd.DataFrame(list(zip(articles, texts)), columns=['article', 'text'])
-    click_stream_df = pd.read_csv(fdf)
+    click_stream_dataset = filter_available_titles(click_stream, available_titles)
+    click_stream_dataset = add_metrics(click_stream_dataset)
+    click_stream_dataset.columns = ['previous_article', 'current_article', 'number_of_clicks', 'click_rate']
 
-    click_stream_df = click_stream_df.astype('object')
+    return click_stream_dataset
 
-    merged_df = pd.merge(click_stream_df, articles_df, left_on='previous_article', right_on='article')
-    merged_df = pd.merge(merged_df, articles_df, left_on=['current_article'], right_on=['article'])
-    merged_df = merged_df.drop(columns=['article_x', 'article_y'])
-    merged_df.columns = ['previous_article', 'current_article', 'number_of_clicks', 'click_rate',
+
+def combine_wiki_click_stream_datasets(click_stream_dataset, wiki_documents_dataset):
+    combined_dataset = pd.merge(click_stream_dataset, wiki_documents_dataset, left_on=['previous_article'], right_on=['article'])
+    combined_dataset = pd.merge(combined_dataset, wiki_documents_dataset, left_on=['current_article'], right_on=['article'])
+    combined_dataset = combined_dataset.drop(columns=['article_x', 'article_y'])
+    combined_dataset.columns = ['previous_article', 'current_article', 'number_of_clicks', 'click_rate',
                          'previous_article_text', 'current_article_text']
-    merged_df = merged_df[
+    combined_dataset = combined_dataset[
         ['previous_article', 'previous_article_text', 'current_article', 'current_article_text', 'number_of_clicks',
          'click_rate']]
 
-    return merged_df
+    return combined_dataset
+
+
+def get_wiki_documents_from_json(wiki_documents_path):
+    texts, labels, articles = [], [], []
+
+    with open(wiki_documents_path, 'r') as json_file:
+        json_list = list(json_file)
+    for json_str in json_list:
+        result = json.loads(json_str)
+        texts.append(result['sections'][0]['paragraphs'])
+        articles.append(result['title'])
+
+    wiki_documents_dataset = pd.DataFrame(list(zip(articles, texts)), columns=['article', 'text'])
+
+    return wiki_documents_dataset
+
+
+def generate_dataset(click_stream_dump_path, wiki_documents_path):
+    """
+    Process:
+        - Download Simple English data XML (not implemented)
+        - Transform to JSON using wiki_cli.py (not implemented)
+        - Download click stream (not implemented)
+        - Generate click stream dataset for titles available on Simple English - ok
+        - Combine click stream dataset with JSON dataset
+        - Save result as CSV
+
+    :return: void
+    """
+    print('Generating dataset')
+
+    click_stream_dataset = generate_click_stream_dataset(click_stream_dump_path, wiki_documents_path)
+    wiki_documents_dataset = get_wiki_documents_from_json(wiki_documents_path)
+
+    combined_dataset = combine_wiki_click_stream_datasets(click_stream_dataset, wiki_documents_dataset)
+
+    return combined_dataset
 
 
 if __name__ == '__main__':
@@ -90,12 +124,15 @@ if __name__ == '__main__':
 
     print('Running main')
 
-    cs_dump_path = '../data/clickstream-enwiki-2019-08.tsv'
-    docs_path = '../data/simplewiki.jsonl'
+    click_stream_dump_path = '../data/clickstream-enwiki-2019-08.tsv'
+    wiki_documents_path = '../data/simplewiki.jsonl'
 
-    wiki_df = add_articles_data('../data/simplewiki.jsonl', '../data/click_pair_small.csv')
+    LOG_FORMAT = '[%(asctime)s] [%(levelname)s] %(message)s (%(funcName)s@%(filename)s:%(lineno)s)'
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
-    wiki_df.to_csv('../data/wiki_df_small.csv', index=False)
+    wiki_dataset = generate_dataset(click_stream_dump_path, wiki_documents_path)
+
+    print(wiki_dataset.sample(n=10))
 
     # LOG_FORMAT = '[%(asctime)s] [%(levelname)s] %(message)s (%(funcName)s@%(filename)s:%(lineno)s)'
     # logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
--- han_wiki_data/src/utils.py
+++ han_wiki_data/src/utils.py
@@ -5,7 +5,6 @@ import torch
 import sys
 import csv
 import numpy as np
-import json
 import pandas as pd
 import logging
 from sklearn import metrics
@@ -74,81 +73,14 @@ def get_max_lengths(data_path):
     return max(word_length_list), max(sent_length_list), max(paragraph_length_list)
 
 
-def read_click_stream_dump(click_stream_dump_path):
-    rows = []
-
-    with open(click_stream_dump_path, 'r') as f:
-        for i, line in enumerate(f):
-            cols = line.split('\t')
-
-            if cols[2] == 'link':  # type
-                rows.append([
-                    cols[0],  # prev
-                    cols[1],  # current
-                    int(cols[3]),  # n
-                ])
-
-    # TODO use dumps from more months
-
-    return pd.DataFrame(rows, columns=['prev', 'current', 'n'])
-
-
-def get_available_titles(dataset_path):
-    # Load preprocessed
-    title2sects = {}
-
-    with open(dataset_path, 'r') as f:
-        for i, line in enumerate(f):
-            doc = json.loads(line)
-            title = doc['title'].replace(' ', '_')
-            title2sects[title] = [sect['paragraphs'] for sect in doc['sections']]
-
-    print(f'Completed after {i} lines')
-
-    return set(title2sects.keys())  # save as set (makes it faster)
-
-
-def normalize_dataset(cdf, available_titles):
-    # Clicks for that we have matching articles
-    fdf = cdf[(cdf['prev'].isin(available_titles)) & (cdf['current'].isin(available_titles))].copy()
-
-    print(f'Click pairs with articles: {len(fdf):,}')
-
-    max_n = fdf.groupby(['prev']).agg({'n': 'max'})
-
-    # Normalize click count with max value
-    fdf['rel_n'] = 0.
-
-    for idx, r in fdf.iterrows():
-        fdf.at[idx, 'rel_n'] = r['n'] / max_n['n'][r['prev']]
-
-    return fdf
-
-
-def dataset_creation(click_stream_dump_path, dataset_path):
-    cdf = read_click_stream_dump(click_stream_dump_path)
-
-    print(f'Total click pairs: {len(cdf):,}')
-
-    available_titles = get_available_titles(dataset_path)
-
-    fdf = normalize_dataset(cdf, available_titles)
-
-    print(fdf.sample(n=10))
-
-    fdf.to_csv('../data/click_pair.csv',
-               index=False,
-               header=['previous_article', 'current_article', 'number_of_clicks', 'click_rate'])
-
-
 if __name__ == "__main__":
-    word, sent, paragraph = get_max_lengths('../data/wiki_df_small.csv')
-    print(word)
-    print(sent)
-    print(paragraph)
+    # word, sent, paragraph = get_max_lengths('../data/wiki_df_small.csv')
+    # print(word)
+    # print(sent)
+    # print(paragraph)
 
-    cs_dump_path = '../data/clickstream-enwiki-2019-08.tsv'
-    docs_path = '../data/simplewiki.jsonl'
+    click_stream_dump_path = '../data/clickstream-enwiki-2019-08.tsv'
+    wiki_documents_path = '../data/simplewiki.jsonl'
 
     LOG_FORMAT = '[%(asctime)s] [%(levelname)s] %(message)s (%(funcName)s@%(filename)s:%(lineno)s)'
     logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
